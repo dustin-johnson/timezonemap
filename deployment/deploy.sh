91c2adc51260235f56984f96ae4975d35a4a689c
Dustin Johnson <dustin@dustinj.us>
2018-12-20 14:32:54 +0100

    Debugging

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     echo "TestEnv: ${TEST_ENV:1}"
+    GPG_PASSPHRASE2=${GPG_PASSPHRASE:1}
+    echo GPG_PASSPHRASE2
     mvn deploy -P sign,build-extras --settings deployment/maven_settings.xml
 fi
