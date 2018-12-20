8a6719d2eca7abc23288b4d5a99b2ce61d1934e6
Dustin Johnson <dustin@dustinj.us>
2018-12-21 00:47:58 +0100

    Simplify deployment scripts. Deploy release branches as well as the master branch.

--- deployment/before-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_3253fed51ff9_key -iv $encrypted_3253fed51ff9_iv -in deployment/codesigning.asc.enc -out deployment/codesigning.asc -d
-    gpg --fast-import deployment/codesigning.asc
-fi
--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    echo "TestEnv: ${TEST_ENV:1}"
+
+if ([ $BRANCH = master ] || [[ $BRANCH = release/* ]]) && [ $PULL_REQUEST = false ]; then
+    openssl aes-256-cbc -K $encrypted_3253fed51ff9_key -iv $encrypted_3253fed51ff9_iv -in deployment/codesigning.asc.enc -out deployment/codesigning.asc -d
+    gpg --fast-import deployment/codesigning.asc
+
     mvn deploy -P sign,build-extras --settings deployment/maven_settings.xml
 fi
