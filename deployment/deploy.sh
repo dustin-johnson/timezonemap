daecc5c022ee10a5f591ab8742e66010a11bbd39
Dustin Johnson <dustin@dustinj.us>
2018-12-20 14:58:31 +0100

    Update gpg passphrase

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     echo "TestEnv: ${TEST_ENV:1}"
-    export GPG_PASSPHRASE2=${GPG_PASSPHRASE:1}
-    echo $GPG_PASSPHRASE2
     mvn deploy -P sign,build-extras --settings deployment/maven_settings.xml
 fi
