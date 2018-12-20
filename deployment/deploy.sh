0db3af9a46b28e557820c387422d45bc264375fb
Dustin Johnson <dustin@dustinj.us>
2018-12-20 13:41:43 +0100

    Substring the debug output to avoid masking

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    echo "TestEnv: $TEST_ENV"
+    echo "TestEnv: ${TEST_ENV:1}"
     mvn deploy -P sign,build-extras --settings deployment/maven_settings.xml
 fi
