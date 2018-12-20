cd863cc22cac0431f1e31c42a3b3598ba124b4e8
Dustin Johnson <dustin.johnson@seeq.com>
2018-12-20 10:55:37 +0100

    Correct path

--- deployment/before-deploy.sh
+++ deployment/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_839e06c05d8b_key -iv $encrypted_839e06c05d8b_iv -in deployment/codesigning.asc.enc -out deployment/codesigning.asc -d
-    gpg --fast-import cd/codesigning.asc
+    gpg --fast-import deployment/codesigning.asc
 fi
