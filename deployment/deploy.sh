2c3e0390d3df95dad527a88cc646230d098ea934
Dustin Johnson <dustin@dustinj.us>
2018-12-21 02:03:02 +0100

    Add quotes to deploy script if statement

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if ([ "$BRANCH" == master ] || [[ "$BRANCH" == release/* ]]) && [ "$PULL_REQUEST" == false ]; then
+if ([ "$BRANCH" = master ] || [[ "$BRANCH" = release/* ]]) && [ "$PULL_REQUEST" = false ]; then
     openssl aes-256-cbc -K $encrypted_3253fed51ff9_key -iv $encrypted_3253fed51ff9_iv -in deployment/codesigning.asc.enc -out deployment/codesigning.asc -d
     gpg --fast-import deployment/codesigning.asc
 
