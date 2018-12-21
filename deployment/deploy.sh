6b754f6d570f79866d1202b77ff756aed39b23ed
Dustin Johnson <dustin@dustinj.us>
2018-12-21 02:14:21 +0100

    Add test output

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 echo "Deploy script running"
+
+if [[ "$BRANCH" = release/* ]]; then
+    echo "test1 - pass"
+else
+    echo "test1 - fail"
+fi
+
+if ([ "$BRANCH" = master ] || [[ "$BRANCH" = release/* ]];) then
+    echo "test1 - pass"
+else
+    echo "test1 - fail"
+fi
+
 if ([ "$BRANCH" = master ] || [[ "$BRANCH" = release/* ]]) && [ "$PULL_REQUEST" = false ]; then
     openssl aes-256-cbc -K $encrypted_3253fed51ff9_key -iv $encrypted_3253fed51ff9_iv -in deployment/codesigning.asc.enc -out deployment/codesigning.asc -d
     gpg --fast-import deployment/codesigning.asc
