a2a1b5555001612da4d78db11d3c1b12a272732e
Dustin Johnson <dustin@dustinj.us>
2018-12-21 02:07:17 +0100

    Add debug output

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-
+echo "Deploy script running"
 if ([ "$BRANCH" = master ] || [[ "$BRANCH" = release/* ]]) && [ "$PULL_REQUEST" = false ]; then
     openssl aes-256-cbc -K $encrypted_3253fed51ff9_key -iv $encrypted_3253fed51ff9_iv -in deployment/codesigning.asc.enc -out deployment/codesigning.asc -d
     gpg --fast-import deployment/codesigning.asc
 
     mvn deploy -P sign,build-extras --settings deployment/maven_settings.xml
 fi
+echo "Deploy script finished"
