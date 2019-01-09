ef416a3020cb94b60a71fcb1bdf3144f84cdbc66
Dustin Johnson <dustin@dustinj.us>
2019-01-10 01:53:12 +0200

    Deploy version tags and release branch builds that are building a snapshot version

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 echo "Deploy script running"
 
-if ([ "$TRAVIS_BRANCH" = master ] || [[ "$TRAVIS_BRANCH" = release/* ]]) && [ "$TRAVIS_PULL_REQUEST" = false ]; then
+# Deploy if the branch is a tagged release (v3.1, etc.) or if the branch is a snapshot version on a release branch.
+if [[ "$TRAVIS_BRANCH" =~ v[0-9](\.[0-9])+ ]] || ([[ "$TRAVIS_BRANCH" = release/* ]] && compgen -G "timezonemap/target/timezonemap-*-SNAPSHOT.jar" > /dev/null); then
     openssl aes-256-cbc -K $encrypted_3253fed51ff9_key -iv $encrypted_3253fed51ff9_iv -in deployment/codesigning.asc.enc -out deployment/codesigning.asc -d
     gpg --fast-import deployment/codesigning.asc
 
