db8f5da90e60a20d0f86123a68cfdbb5930f4d84
Dustin Johnson <dustin@dustinj.us>
2019-01-10 11:26:58 +0200

    Update version to v3.2

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-echo "Deploy script running"
 
 # Deploy if the branch is a tagged release (v3.1, etc.) or if the branch is a snapshot version on a release branch.
 if [[ "$TRAVIS_BRANCH" =~ v[0-9](\.[0-9])+ ]] || ([[ "$TRAVIS_BRANCH" = release/* ]] && compgen -G "timezonemap/target/timezonemap-*-SNAPSHOT.jar" > /dev/null); then
     openssl aes-256-cbc -K $encrypted_3253fed51ff9_key -iv $encrypted_3253fed51ff9_iv -in deployment/codesigning.asc.enc -out deployment/codesigning.asc -d
     gpg --fast-import deployment/codesigning.asc
 
-    mvn deploy -DskipDataPackaging=true -DskipTests=true -Dmaven.javadoc.skip=true -P sign,build-extras --settings deployment/maven_settings.xml
+    mvn deploy -DskipDataPackaging=true -DskipTests=true -P sign,build-extras --settings deployment/maven_settings.xml
+else
+    echo "Deploy skipped"
 fi
