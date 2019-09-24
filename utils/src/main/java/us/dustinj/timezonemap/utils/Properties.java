0ca68724cb61a0b2666dcc3296f46c8c93172c84
eugene-galkine <eugene.galkine@gmail.com>
2019-09-24 12:46:47 -0500

    refactored to remove Java 8 specific code

--- utils/src/main/java/us/dustinj/timezonemap/utils/Properties.java
+++ utils/src/main/java/us/dustinj/timezonemap/utils/Properties.java
@@ -1,9 +1,11 @@
 package us.dustinj.timezonemap.utils;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.AbstractMap;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -13,13 +15,28 @@ public final class Properties {
 
     public static Map<String, String> getProperties(Class<?> resourceOwner, String filename) {
         InputStream inputStream = resourceOwner.getResourceAsStream("/" + filename);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        Map<String, String> map = new HashMap<>();
+        String line;
 
-        return new BufferedReader(new InputStreamReader(inputStream)).lines()
-                .map(String::trim)
-                .filter(line -> !line.startsWith("#"))
-                .map(line -> line.split("=", 2))
-                .map(lineFragments -> new AbstractMap.SimpleEntry<>(lineFragments[0],
-                        lineFragments[1].replace("\\", "")))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        try {
+            while ((line = reader.readLine()) != null) {
+                line = line.trim();
+                if (!line.startsWith("#")) {
+                    String[] split = line.split("=", 2);
+                    map.put(split[0], split[1].replace("\\", ""));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return map;
     }
 }
